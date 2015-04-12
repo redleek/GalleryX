c5fb54654200a1facd218afbe3a6c5cdd1ef42cc
Connor Blakey <connorblakey96@gmail.com>
2015-04-12 03:28:23 +0100

    Added some documentation.

--- GalleryX/MainWindow.xaml.cs
+++ GalleryX/MainWindow.xaml.cs
@@ -23,42 +23,41 @@ namespace GalleryX
     {
         private void Test()
         {
-            Artist Fred = new Artist("Fred", 0);
-            //Artwork newArt = new Artwork("New artwork", 30, DateTime.Now, ArtworkState.InGallery, 0);
-            Artwork newArt = new Artwork(
-                "New artwork",
-                30,
-                DateTime.Now,
-                Artwork.ArtworkType.Painting,
-                0
+            Artist artist = new Artist("Rob Miles", 32);
+            Artwork artwork1, artwork2, artwork3;
+            artist.AddArtwork(
+                artwork1 = new Artwork(
+                    "Hay",
+                    319.99m,
+                    DateTime.Now,
+                    Artwork.ArtworkType.Painting,
+                    Artwork.ArtworkState.InGallery)
+                    );
+            artist.AddArtwork(
+                artwork2 = new Artwork(
+                    "Way",
+                    259.99m,
+                    DateTime.Now,
+                    Artwork.ArtworkType.Sculpture,
+                    Artwork.ArtworkState.InGallery)
+                    );
+            artist.AddArtwork(
+                artwork3 = new Artwork(
+                    "Nay",
+                    169.99m,
+                    DateTime.Now,
+                    Artwork.ArtworkType.Sculpture,
+                    Artwork.ArtworkState.AwaitingGalleryEntry)
                 );
-            Fred.AddArtwork(newArt);
-            Fred.AddArtwork(new Artwork(
-                "asd",
-                34,
-                DateTime.Now,
-                Artwork.ArtworkType.Sculpture,
-                1
-                ));
-            Fred.AddArtwork(new Artwork(
-                "sdada",
-                234,
-                DateTime.Now,
-                Artwork.ArtworkType.Painting,
-                2
-                ));
-            //newArt.Save("newArt.txt");
-            Fred.Save("GalleryX.txt");
-            Console.WriteLine(newArt.ToString());
-
-            Artist loadedArtist = Artist.Load("GalleryX.txt");
-            //Artwork loadedArt = Artwork.Load("newArt.txt");
+            artist.Save("test_save.txt");
         }
 
         public MainWindow()
         {
             InitializeComponent();
-            Test();
+            Title = "GalleryX";
+            //Test();
+            Artist loadedArtist = Artist.Load("test_save.txt");
         }
     }
 }
