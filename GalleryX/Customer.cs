bc559750101f34cc8cbd774c9754c098c5d2417e
Connor Blakey <connorblakey96@gmail.com>
2015-04-29 18:05:13 +0100

    Implemented Order class without ID property as of yet. Made basic Customer class.

--- GalleryX/Customer.cs
+++ GalleryX/Customer.cs
@@ -9,5 +9,79 @@ namespace GalleryBusiness
 {
     class Customer
     {
+        /// <summary>
+        /// Basic Exception class for Customers.
+        /// </summary>
+        class CustomerException : System.Exception
+        {
+            public CustomerException(string message)
+                : base(message)
+            { }
+        }
+
+        /// <summary>
+        /// Incorrect name Exception.
+        /// </summary>
+        class CustomerExceptionBadName : CustomerException
+        {
+            public CustomerExceptionBadName(string message)
+                : base(message)
+            { }
+        }
+
+        /// <summary>
+        /// Maximum allowed characters in Name.
+        /// </summary>
+        public const int MAX_NAME_CHARS = 20;
+
+        private string mName;
+        private Dictionary<int, Order> mOrders;
+        private Gallery mGallery;
+
+        /// <summary>
+        /// Create a new instance of a Customer loaded from file.
+        /// </summary>
+        /// <param name="inName">Name of the Customer.</param>
+        /// <param name="inOrders">Dictionary of Orders of the Customer.</param>
+        /// <param name="inGallery">Reference to the Gallery where the Orders are being made.</param>
+        private Customer(string inName, Dictionary<int, Order> inOrders, Gallery inGallery)
+        {
+            if (!UpdateName(inName))
+            {
+                throw new CustomerExceptionBadName("Name is blank.");
+            }
+            mOrders = inOrders;
+            mGallery = inGallery;
+        }
+
+        /// <summary>
+        /// Create a new instance of a Customer.
+        /// </summary>
+        /// <param name="inName">Name of the Customer.</param>
+        /// <param name="inGallery">Reference to the Gallery where the Orders are made.</param>
+        public Customer(string inName, Gallery inGallery)
+            : this(inName, new Dictionary<int, Order>(), inGallery)
+        { }
+
+        /// <summary>
+        /// Update the name of the Customer.
+        /// </summary>
+        /// <param name="pNewName">New name of the Customer.</param>
+        /// <returns>Returns whether or not the name is valid.</returns>
+        private bool UpdateName(string pNewName)
+        {
+            pNewName = pNewName.Trim();
+            if (pNewName != "")
+            {
+                if (pNewName.Length <= MAX_NAME_CHARS)
+                {
+                    mName = pNewName;
+                    return true;
+                }
+                throw new CustomerExceptionBadName("Name length is too long by "
+                    + (pNewName.Length - MAX_NAME_CHARS) + " characters.");
+            }
+            return false;
+        }
     }
 }
---
+++ GalleryX/Order.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GalleryBusiness
+{
+    /// <summary>
+    /// An Order for an Artwork made by a Customer.
+    /// </summary>
+    class Order
+    {
+        private int mArtworkID;
+        private DateTime mOrderDate;
+        private Customer mCustomer;
+
+        /// <summary>
+        /// Create a new instance of an Order.
+        /// </summary>
+        /// <param name="inArtworkID">Unique ID of the ordered Artwork.</param>
+        /// <param name="inOrderDate">Order date of the Order.</param>
+        /// <param name="inCustomer">Reference to the Customer who ordered the Artwork.</param>
+        public Order(int inArtworkID, DateTime inOrderDate, Customer inCustomer)
+        {
+            mArtworkID = inArtworkID;
+            mOrderDate = inOrderDate;
+            mCustomer = inCustomer;
+        }
+
+        /// <summary>
+        /// The ID of the ordered Artwork.
+        /// </summary>
+        public int ArtworkID
+        {
+            get { return mArtworkID; }
+        }
+
+        /// <summary>
+        /// Date of the order of the Artwork.
+        /// </summary>
+        public string OrderDate
+        {
+            get { return mOrderDate.ToString(); }
+        }
+
+        /*
+        public int ID
+        {
+            get { return }
+        }
+        */
+
+        public void XmlSave(XmlTextWriter pXmlOut)
+        {
+            pXmlOut.WriteStartElement("Order");
+            pXmlOut.WriteEndElement();
+            throw new NotImplementedException("Needs Order ID.");
+        }
+
+        /// <summary>
+        /// Gets the string value of the Order.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "ID of Artwork: " + mArtworkID + ", Date of Order: " + OrderDate;
+        }
+
+        /// <summary>
+        /// Checks if the passed object has the same content.
+        /// </summary>
+        /// <param name="obj">Object to compare.</param>
+        /// <returns>Whether or not the two objects are equal.</returns>
+        public override bool Equals(object obj)
+        {
+            Order CompareOrder = (Order)obj;
+
+            bool ArtworkID = (mArtworkID == CompareOrder.mArtworkID);
+            bool OrderDate = (mOrderDate == CompareOrder.mOrderDate);
+
+            if (ArtworkID && OrderDate)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
